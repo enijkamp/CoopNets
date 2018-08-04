42591383fb10a4124ec50f6fc7fc7bb49fb310d7
enijkamp <erik.nijkamp@gmail.com>
2018-08-03 17:07:36 -0700

    delta-orthogonal

--- model/model.py
+++ model/model.py
@@ -223,14 +223,9 @@ class CoopNets(object):
 
     def descriptor(self, inputs):
         with tf.variable_scope('des', reuse=tf.AUTO_REUSE):
-            conv1 = conv2d(inputs, 64, kernal=(5, 5), strides=(2, 2), padding="SAME", activate_fn=leaky_relu,
-                           name="conv1")
-
-            conv2 = conv2d(conv1, 128, kernal=(3, 3), strides=(2, 2), padding="SAME", activate_fn=leaky_relu,
-                           name="conv2")
-
-            conv3 = conv2d(conv2, 256, kernal=(3, 3), strides=(1, 1), padding="SAME", activate_fn=leaky_relu,
-                           name="conv3")
+            conv1 = conv2d(inputs, 64, kernal=(5, 5), strides=(2, 2), padding="SAME", activate_fn=tf.nn.tanh, name="conv1")
+            conv2 = conv2d(conv1, 128, kernal=(3, 3), strides=(2, 2), padding="SAME", activate_fn=tf.nn.tanh, name="conv2")
+            conv3 = conv2d(conv2, 256, kernal=(3, 3), strides=(1, 1), padding="SAME", activate_fn=tf.nn.tanh, name="conv3")
 
             fc = fully_connected(conv3, 100, name="fc")
 
